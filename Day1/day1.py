e8b0a155405279462adb46046234373c179758b2
Jessica <jessicakrishtul@gmail.com>
2021-12-01 11:11:24 -0500

    add input file & state problem

---
+++ Day1/day1.py
@@ -0,0 +1,16 @@
+"""
+
+--- Day 1: Sonar Sweep ---
+
+As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor.
+On a small screen, the sonar sweep report (your puzzle input) appears:
+each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
+
+The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with
+- you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
+
+To do this, count the number of times a depth measurement increases from the previous measurement.
+(There is no measurement before the first measurement.)
+
+"""
+
