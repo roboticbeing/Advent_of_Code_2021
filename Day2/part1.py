58422fa12f756bc0756e8740c93b38d4a0a69558
Jessica <jessicakrishtul@gmail.com>
2021-12-02 17:38:34 -0500

    Day 2 - Part 1 - Complete

---
+++ Day2/part1.py
@@ -0,0 +1,38 @@
+"""
+
+--- Day 2: Dive! ---
+
+Now, you need to figure out how to pilot this thing.
+
+It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
+
+forward X increases the horizontal position by X units.
+down X increases the depth by X units.
+up X decreases the depth by X units.
+Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
+
+"""
+
+# Add the input file into a list
+input_file = open('input.txt', 'r')
+input_list = []
+
+for line in input_file:
+    # Get rid of new line characters in the list and split
+    input_list.append(line.replace('\n', '').split())
+
+# variable declarations
+horizontal_position = 0
+depth = 0
+
+for i in range(len(input_list)):
+    if input_list[i][0] == 'forward':
+        # Cast to int
+        horizontal_position += int(input_list[i][1])
+    elif input_list[i][0] == 'down':
+        depth += int(input_list[i][1])
+    else:
+        depth -= int(input_list[i][1])
+
+print('The product of my final horizontal position and my final depth is', horizontal_position * depth)
+
